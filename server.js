74412821df9c3234db536be3daa65235377893e6
Carlos Romellon <crromellon@outlook.com>
2024-04-29 20:19:13 -0600

    Dios mio, solo esta azul
    No sirve todavia, ya que como no le se a node js pos no sirve el boton xd

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // Medios de inicializacion de las librerias
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = require('./controllers/routes');
 
+// Configuración de la plantilla Pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
-    app.get('/', (req, res) => {
-        res.send('Buenas noches viva dios, y asi cosas');
-    });
+// Middleware para procesar archivos estáticos en la carpeta 'public'
+app.use(express.static('public'));
+app.use(express.json());
+
+app.use('/', router);
+
 // Puerto en el que escucha el servidor y se muestra
 const PORT = 3000;
 app.listen(PORT, () => {
