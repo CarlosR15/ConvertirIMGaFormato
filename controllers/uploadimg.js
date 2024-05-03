14b1c65f25cd9b41304a8f2453cea1b8b424fb44
Carlos Romellon <crromellon@outlook.com>
2024-05-02 18:50:05 -0600

    modificar cosas pa que jale
    await y async en pla

--- controllers/uploadimg.js
+++ controllers/uploadimg.js
@@ -3,10 +3,26 @@ const router = express.Router();
 
 //multer para subir imagenes
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+
+//sharp para lo de detectar el que tipo de imagen
+const sharp = require('sharp')
+
+//probar hacerlo async para hacer lo de abajo await
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb (null, './uploads')
+  },
+  filename: (req, file, cb) => {
+    const ext = file.originalname.split('.').pop()
+    cb(null, `${Date.now()}.${ext}`)
+  }
+})
+
+//hacer esto await
+const upload = multer({ storage })
 
 router.post('/', upload.single('file'), (req, res) => {
-  res.json(req.file);
+  res.redirect('/');
 });
 
 module.exports = router;
